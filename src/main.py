c104f98e0588ae45b1617fc22fcd4196f67157d7
Philipp Schubaur <philipp.schubaur@hs-augsburg.de>
2021-03-12 17:23:54 +0100

    working on thingspeak update

--- src/main.py
+++ src/main.py
@@ -2,16 +2,29 @@ import thingspeak
 import time
 from pythonping import ping
 
-from credentials import channel_id
-from credentials import write_key
-from credentials import read_key
 
 
-def measure():
-    response_list = ping('www.google.de', size=40, count=3, timeout=3)
-    print(response_list.rtt_avg_ms)
+def measure(channel):
+    try:
+        response_list = ping('www.google.de', size=40, count=3, timeout=3)
+        ping_median = response_list.rtt_avg_ms
+        print(ping_median)
+        # write
+        response = channel.update({'field1': ping_median})
+        print(response)
+
+    except:
+        print("connection failed")
+
+
     # write
     #response = channel.update({'field1': ping})
 
 if __name__ == "__main__":
-    measure()
+    from credentials import *
+    channel = thingspeak.Channel(id=CHANNEL_ID, api_key=READ_KEY)
+    measure(channel)
+    # while True:
+    #     measure(channel)
+    #     # free account has an api limit of 15sec
+    #     time.sleep(15)
